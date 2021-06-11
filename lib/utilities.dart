c565a41c9390a73be66bdfd79fe41f70ce868a34
wafds <christian.eichmueller@dayblizz.com>
2021-06-11 12:07:04 +0200

    fine tuning of the utilities class

--- lib/utilities.dart
+++ lib/utilities.dart
@@ -1,13 +1,8 @@
 import 'package:spell_checker/constants.dart';
 
-/// Determines whether parameter is null
-bool isNull(Object obj) {
-  return obj == null;
-}
-
 /// Determines whether parameter is a character
-bool isChar(int char) {
-  return !isNull(char) &&
+bool isChar(int? char) {
+  return !(char==null) &&
       char >= CHARACTER_RANGE_LOW &&
       char <= CHARACTER_RANGE_HIGH;
 }
